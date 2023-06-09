dfaccaa73172f217d026e351c88e2c514f80d847
Saurav <er.sauravgpt@gmail.com>
2023-06-09 12:26:54 +0530

    Created: aboutUs single box layout

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,23 +1,32 @@
-
+import { alcom_logo } from "./images";
 export default function AboutUs() {
   return (
 
     <>
-      <div className="mt-32 ">
-        <h1>this is the about page</h1>
-        <div className="display flex justify-center  bg-purple-400  h-96">
+      <div className="bg-blue-200 border-4 flex-col border-purple-500 h-[1500px] py-28">
 
-          <div className="buttons flex h-fit m-auto py-8 ">
 
-          <div className="mx-3 bg-purple-500 h-fit text-xl p-4">container 1</div>
-          <div className="me-8 bg-yellow-400 h-fit text-xl p-4">container 2</div>
-          <div className="me-8 bg-yellow-400 h-fit text-xl p-4">container 3</div>
-          <div className="me-8 bg-yellow-400 h-fit text-xl p-4">container 4</div>
-          </div>
+        <div className="heading text-6xl text-center font-bold">
+          About us
         </div>
 
-        
+
+        <div className="container relative flex   h-fit w-3/4 justify-end  border-4 border-green-500 mt-16 left-96 ">
+
+          <article className="relative w-96 h-[20rem] right-52 top-20">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt quia qui amet esse.
+            Impedit quod vitae laborum architecto debitis, sapiente eligendi repellat a repellendus
+            quisquam quos unde, odit laboriosam aperiam ipsa corrupti Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Earum officia culpa nihil rerum ipsam modi nisi tempore illum voluptatem? Enim s
+            ed quia voluptates! Corporis temporibus nesciunt unde minima aspernatur et.
+          </article>
+
+          <div className="relative h-fit border-4 border-gray-800 right-9">
+            <img src={alcom_logo} className="h-[28rem] w-[35 rem] " alt="" />
+          </div>
+        </div>
       </div>
+
     </>
   )
 }
