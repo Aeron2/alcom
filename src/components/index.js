9888158ffaae62b5c12ab5fc05f02780f5640c69
Saurav <er.sauravgpt@gmail.com>
2023-06-09 19:12:21 +0530

    homepage imported

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
-import AboutUs from './AboutUs'
-export { Navbar, Hero , AboutUs};
+import AboutUs from "./aboutUs";
+import Homepage from "./Homepage";
+export { Navbar, Hero , AboutUs,Homepage};
