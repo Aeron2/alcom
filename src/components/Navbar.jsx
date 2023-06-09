534a2ed1332b46e010bfd0321481ef5d3b94a18d
Saurav <er.sauravgpt@gmail.com>
2023-06-09 12:28:25 +0530

    ggsipu logo is placed at right corner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import { ggsipu_logo, alcom_logo } from "./images";
 function Navbar() {
   return (
-    <div className="hel px-8 !justify-between  py-2 border-b-2 border-slate-300 shadow-xl rounded-b-3xl fixed top-0  bg-white w-screen ">
+    <div className="hel z-50 px-8  py-2 border-b-2  border-slate-300 shadow-xl rounded-b-3xl fixed top-0  bg-white w-screen ">
       <div>
         {/* alcom logo */}
         <img src={alcom_logo} alt="alcom logo" className="h-20 cursor-pointer" />
       </div>
-      <div>
+      <div className="ml-5">
         {/* linkds */}
-        <ul className="hel space-x-6 text-lg text-[#3e7fa6] ">
+        <ul className="hel space-x-9 text-lg text-[#3e7fa6] ">
           <li className="nav-links ">About Us</li>
           <li className="nav-links ">Our Team</li>
           <li className="nav-links ">Our Work</li>
@@ -16,7 +16,7 @@ function Navbar() {
           <li className="nav-links ">Connect With Us</li>
         </ul>
       </div>
-      <div>
+      <div className="ml-auto">
         {/* ggsipu logo */}
         <img src={ggsipu_logo} alt="ggsipu logo" className="h-20 cursor-pointer" />
       </div>
